318ce70535bf9868928bf4b9856f87d940ad0d66
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-10 05:15:48 +0500

    Add files via upload

---
+++ simple for loop statement.cpp
@@ -0,0 +1,17 @@
+// program to display a text 100 times
+#include <iostream>      
+//preprocessor directive
+using namespace std;
+int main()             
+//main function
+{                     
+ //opening braces
+	int y=0;          
+	 //declare a variable and assign it a value
+	for(y=0;y<=100;y++)  
+	//run a loop for 100 times 
+	cout<<"Hello World"<<endl;
+	//print hello world 100 times
+	return 0;
+}
+//cloding braces
