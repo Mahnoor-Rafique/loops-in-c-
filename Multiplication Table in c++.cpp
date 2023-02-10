73a4f31e9c63c9fbae7b792261825f483ba59b46
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-10 05:15:25 +0500

    Add files via upload

---
+++ Multiplication Table in c++.cpp
@@ -0,0 +1,22 @@
+//c++ program to display table of 14
+#include <iostream>      
+//preprocessor directive
+using namespace std;
+int main()             
+//main function
+{                     
+ //opening braces
+ int j,k; 
+ //declare two variable
+cout<<"Enter any number:";
+//print the value of k 
+cin>>k;
+//take the input from user
+for(j=1;j<=10;j++)
+//run loop for 10 times
+cout<<k <<"*"<<j <<"="<<k*j<<endl;
+//print until the given condition becomes false
+ return 0;
+}
+//closing braces
+ 
