decd9fb5cc561845cf717add43e91e83d7081a08
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-22 20:30:53 +0500

    Add files via upload

---
+++ Simple while loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//preprocessor directive
+using namespace std;
+
+int main()
+//main function
+ {
+ 	int i=1;//initialization
+ 	while(i<=5)
+ 	//loop 
+	 {
+	 
+ 	cout<<"Hello World!" <<endl;
+ 	//display text until the condition becomes false
+ 	i++;
+ 	//incrementer
+ }
+ 	return 0;
+ }
